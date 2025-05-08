8cb840fe04ea8c255f08b8c9df9c77fa250c49ec
kitsune2355 <pe3ka.63@gmail.com>
2025-05-08 13:03:57 +0700

    Add pagination

--- src/screens/Dashboard/DashboardScreen.tsx
+++ src/screens/Dashboard/DashboardScreen.tsx
@@ -1,9 +1,9 @@
-import React, { useCallback, useEffect } from "react";
+import React, { useCallback, useEffect, useState } from "react";
 import { KnowledgeCard } from "../../components/KnowledgeCard";
 import { useDispatch, useSelector } from "react-redux";
 import { AppDispatch, RootState } from "../../store";
 import { fetchPosts } from "../../redux/actions/postActions";
-import { Divider } from "antd";
+import { Divider, Pagination } from "antd";
 
 export const findCategory = (
   categoryList: any[],
@@ -47,6 +47,8 @@ export const DashboardScreen: React.FC = () => {
   const selectedTags = useSelector(
     (state: RootState) => state.search.selectedTags
   );
+  const [currentPage, setCurrentPage] = useState(1);
+  const [pageSize] = useState(9);
 
   const fetchData = useCallback(async () => {
     try {
@@ -76,57 +78,73 @@ export const DashboardScreen: React.FC = () => {
     return post.post_publish === "1" && matchesQuery && matchesSelectedTags;
   });
 
-  return (
-    <>
-      <div className="tw-mb-4">
-        <Divider
-          orientation="left"
-          orientationMargin="0"
-          className="!tw-text-xl !tw-text-primary !tw-font-bold !tw-border-primary"
-        >
-          องค์ความรู้องค์กร
-        </Divider>
+  const startIndex = (currentPage - 1) * pageSize;
+  const paginatedPosts = filteredPosts.slice(startIndex, startIndex + pageSize);
 
-        <div className="tw-w-full tw-flex tw-justify-end tw-text-xs tw-text-gray-400">
-          จำนวนองค์ความรู้ : {filteredPosts.length} ฉบับ
+  return (
+    <div className="tw-flex tw-flex-col tw-justify-between tw-min-h-screen tw-gap-4">
+      <div>
+        <div className="tw-mb-4">
+          <Divider
+            orientation="left"
+            orientationMargin="0"
+            className="!tw-text-xl !tw-text-primary !tw-font-bold !tw-border-primary"
+          >
+            องค์ความรู้องค์กร
+          </Divider>
+
+          <div className="tw-w-full tw-flex tw-justify-end tw-text-xs tw-text-gray-400">
+            จำนวนองค์ความรู้ : {filteredPosts.length} ฉบับ
+          </div>
         </div>
+
+        {paginatedPosts.filter((post) => post.post_publish === "1").length >
+        0 ? (
+          <div className="tw-grid tw-grid-cols-12 tw-gap-4">
+            {paginatedPosts
+              .filter((post) => post.post_publish === "1")
+              .map((item, index) => {
+                const tag = findCategory(categories, item.post_ctg_id);
+                return (
+                  <div
+                    key={index}
+                    className="tw-col-span-12 sm:tw-col-span-6 lg:tw-col-span-4 xl:tw-col-span-4 tw-transition-transform tw-duration-300 tw-transform hover:tw-scale-105 tw-cursor-pointer hover:tw-shadow-2xl"
+                  >
+                    <KnowledgeCard
+                      postId={item.id}
+                      title={highlightText(item.post_title, query)}
+                      description={highlightText(item.post_desc, query)}
+                      tags={tag || ["ไม่มีหมวดหมู่"]}
+                      fName={highlightText(item.post_fname, query)}
+                      lName={highlightText(item.post_lname, query)}
+                      postFormat={item.post_format}
+                      createdAt={highlightText(
+                        item.post_create_at as string,
+                        query
+                      )}
+                      postType={item.post_type}
+                      view={item.post_count}
+                    />
+                  </div>
+                );
+              })}
+          </div>
+        ) : (
+          <div className="tw-w-full tw-h-[90vh] tw-flex tw-justify-center tw-items-center tw-text-gray-500">
+            ยังไม่มีองค์ความรู้องค์กร
+          </div>
+        )}
       </div>
 
-      {filteredPosts.filter((post) => post.post_publish === "1").length > 0 ? (
-        <div className="tw-grid tw-grid-cols-12 tw-gap-4">
-          {filteredPosts
-            .filter((post) => post.post_publish === "1")
-            .map((item, index) => {
-              const tag = findCategory(categories, item.post_ctg_id);
-              return (
-                <div
-                  key={index}
-                  className="tw-col-span-12 sm:tw-col-span-6 lg:tw-col-span-4 xl:tw-col-span-4 tw-transition-transform tw-duration-300 tw-transform hover:tw-scale-105 tw-cursor-pointer hover:tw-shadow-2xl"
-                >
-                  <KnowledgeCard
-                    postId={item.id}
-                    title={highlightText(item.post_title, query)}
-                    description={highlightText(item.post_desc, query)}
-                    tags={tag || ["ไม่มีหมวดหมู่"]}
-                    fName={highlightText(item.post_fname, query)}
-                    lName={highlightText(item.post_lname, query)}
-                    postFormat={item.post_format}
-                    createdAt={highlightText(
-                      item.post_create_at as string,
-                      query
-                    )}
-                    postType={item.post_type}
-                    view={item.post_count}
-                  />
-                </div>
-              );
-            })}
-        </div>
-      ) : (
-        <div className="tw-w-full tw-h-[90vh] tw-flex tw-justify-center tw-items-center tw-text-gray-500">
-          ยังไม่มีองค์ความรู้องค์กร
-        </div>
-      )}
-    </>
+      <Pagination
+        current={currentPage}
+        pageSize={pageSize}
+        total={filteredPosts.length}
+        onChange={(page) => setCurrentPage(page)}
+        showSizeChanger={false}
+        className="tw-mt-8 tw-mb-2"
+        align="center"
+      />
+    </div>
   );
 };
